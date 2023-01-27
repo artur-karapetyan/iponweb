648bbea1f47b2c13d34017a76c023ced0b4f6958
Artur Karapetyan <artur.karapetyan.2002@gmail.com>
2023-01-27 12:07:16 +0400

    HW6

--- HW5/Task3.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to copy the contents of a file to another file
-
-def copy(file1, file2):
-    with open(file1, 'r') as file1, open(file2, 'w+') as file2:
-        file2.write(file1.read())
-
-
-copy('File To Copy.txt', 'file.txt')
