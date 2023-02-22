5e4d8644502ba1323f749b53f47ddf44fe6b1fb4
Artur Karapetyan <artur.karapetyan.2002@gmail.com>
2023-02-22 23:37:21 +0400

    classwork

--- src/ClassWork/CorrectUrl.py
+++ src/ClassWork/CorrectUrl.py
@@ -1,25 +1,35 @@
 from urllib.parse import urlparse, urlunparse
 
-# Input URLs
-url1 = "http://tekwerks.com/"
-url2 = "https://tekwerks.com/contact-us"
-
 
 def right_url(url1, url2):
-    parsed_url1 = urlparse('http://' + url1)
+    parsed_url1 = urlparse(url1)
+    if not parsed_url1.scheme and not parsed_url1.netloc:
+        url1 = 'https://' + url1
+    parsed_url1 = urlparse(url1)
+    if parsed_url1.scheme != 'https':
+        url1 = 'https' + url1[4:]
+    parsed_url1 = urlparse(url1)
+    domain_name = parsed_url1.netloc
+    if domain_name.startswith('www.'):
+        domain_name = domain_name[4:]
 
-    if parsed_url1.netloc[:4] != 'www.':
-        parsed_url1 = parsed_url1._replace(netloc='www.' + parsed_url1.netloc)
-
+    # parse url2 and fill in missing components with url1
     parsed_url2 = urlparse(url2)
     if not parsed_url2.scheme:
         parsed_url2 = parsed_url2._replace(scheme=parsed_url1.scheme)
     if not parsed_url2.netloc:
-        parsed_url2 = parsed_url2._replace(netloc=parsed_url1.netloc)
+        parsed_url2 = parsed_url2._replace(netloc=domain_name)
 
-    final_url = urlunparse(parsed_url2)
+    # construct the full URL
+    if parsed_url2.path.startswith('/'):
+        path = parsed_url2.path
+    else:
+        path = '/' + parsed_url2.path
+    return urlunparse(
+        (parsed_url2.scheme, parsed_url2.netloc, path, parsed_url2.params, parsed_url2.query, parsed_url2.fragment))
 
-    return final_url
 
-
+# Input URLs
+url1 = "www.tekwerks.com/"
+url2 = "contact-us"
 print(right_url(url1, url2))
