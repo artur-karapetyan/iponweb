6dea585fb6cde41cde83e0ee77f1c776edc0323c
Artur Karapetyan <artur.karapetyan.2002@gmail.com>
2023-03-03 18:01:47 +0400

    Homework

--- src/Homeworks/HW8-10/OnlineShop/OnlineShop/settings.py
+++ src/Homeworks/HW8-10/OnlineShop/OnlineShop/settings.py
@@ -148,9 +148,9 @@ REST_FRAMEWORK = {
 
 # Mail settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'artur.karapetyan.2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'fxkseezdeqdplmlf'
+EMAIL_HOST = 'host_mail'
+EMAIL_HOST_USER = 'your_mail'
+EMAIL_HOST_PASSWORD = 'your_password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
